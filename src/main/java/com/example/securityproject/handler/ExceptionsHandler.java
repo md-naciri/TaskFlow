fedc809ea6828cb35e075212e7b6059111e177f0
md_naciri <med.ar.naciri@gmail.com>
2024-01-01 18:58:00 +0100

    Error Handling

---
+++ src/main/java/com/example/securityproject/handler/ExceptionsHandler.java
@@ -0,0 +1,39 @@
+package com.example.securityproject.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionsHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> argumentNotValid(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.stream().map(error -> errors.put(error.getField(), error.getDefaultMessage())).collect(Collectors.toList());
+        //return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity illegalArgument(IllegalArgumentException exception){
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(OperationException.class)
+    public ResponseEntity operationException(OperationException exception){
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/example/securityproject/handler/ResponseHandler.java
@@ -0,0 +1,40 @@
+package com.example.securityproject.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ResponseHandler {
+    public int statusCode;
+    public String timeStamp;
+    public String message;
+    public Object data;
+
+    public ResponseHandler(int statusCode, String message, Object data){
+        this.statusCode = statusCode;
+        this.timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss"));
+        this.message = message;
+        this.data = data;
+    }
+
+    public ResponseHandler (int statusCode, String message){
+        this.statusCode = statusCode;
+        this.timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss"));
+        this.message = message;
+    }
+
+    public static ResponseEntity ok(Object data, String message){
+        return new ResponseEntity<>(new ResponseHandler(HttpStatus.OK.value(), message, data), HttpStatus.OK);
+    }
+    public static ResponseEntity created(Object data, String message){
+        return new ResponseEntity<>(new ResponseHandler(HttpStatus.CREATED.value(), message, data), HttpStatus.CREATED);
+    }
+    public static ResponseEntity notFound(String message){
+        return new ResponseEntity<>(new ResponseHandler(HttpStatus.NOT_FOUND.value(), message), HttpStatus.NOT_FOUND);
+    }
+    public static ResponseEntity badRequest(String message){
+        return new ResponseEntity<>(new ResponseHandler(HttpStatus.BAD_REQUEST.value(), message), HttpStatus.BAD_REQUEST);
+    }
+}
